6845947b4fe3939e5f3c46d101b431437ae01706
unknown <ADMIN@PNVD167.pn.com.vn>
2021-01-11 17:38:04 +0700

    Connect SQL Database with Android Studio Code

---
+++ app/src/main/java/com/example/app/Adapter/PopularAdapter.java
@@ -0,0 +1,78 @@
+package com.example.app.Adapter;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.app.model.Popular;
+import com.example.app.R;
+
+import java.util.List;
+
+import static com.google.android.material.resources.MaterialResources.getDrawable;
+
+public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.PopularViewHolder> {
+
+    private Context context;
+    private List<Popular> popularList;
+
+    public PopularAdapter(Context context, List<Popular> popularList) {
+        this.context =  context;
+        this.popularList = popularList;
+    }
+
+
+
+    @NonNull
+    @Override
+    public PopularViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.popular_recycler_items, parent, false);
+        PopularViewHolder popularViewHolder=new PopularViewHolder(view);
+        return new PopularViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PopularViewHolder holder, final int position) {
+
+        holder.popularName.setText(popularList.get(position).getName());
+
+
+//        Glide.with(context).load(popularList.get(position).getImageUrl()).into(holder.image);
+        Log.d("hinh anh: ",String.valueOf(popularList.get(position).getImageUrl()));
+        int id = context.getResources().getIdentifier(popularList.get(position).getImageUrl(), "drawable", context.getPackageName());
+        holder.image.setImageDrawable(context.getResources().getDrawable(id));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return popularList.size();
+    }
+
+    public static class PopularViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView popularName;
+
+        public PopularViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            popularName = itemView.findViewById(R.id.txNamepo);
+            image = (ImageView) itemView.findViewById(R.id.imgPopular);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/app/FoodDetail.java
@@ -0,0 +1,61 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.app.Object.cart;
+import com.example.app.SQL.SQL;
+
+public class FoodDetail extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView itemName, itemPrice,itemRating;
+
+    int id;
+    String name, price, rating, imageUrl;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getSupportActionBar().hide();
+        setContentView(R.layout.pet_details);
+        Intent intent = getIntent();
+
+        Log.d("btn", intent.getStringExtra("id"));
+        name = intent.getStringExtra("name");
+        price = intent.getStringExtra("price");
+        rating = intent.getStringExtra("rating");
+        imageUrl = intent.getStringExtra("image");
+
+
+        imageView = findViewById(R.id.imgDetais);
+        itemName = findViewById(R.id.txtNamedetals);
+        itemPrice = findViewById(R.id.txtPriceDe);
+
+        int id = this.getResources().getIdentifier(imageUrl, "drawable", this.getPackageName());
+        imageView.setImageDrawable(this.getResources().getDrawable(id));
+
+        itemName.setText(name);
+        itemPrice.setText(price+"VND");
+        itemRating.setText(rating);
+        SQL sql= new SQL(FoodDetail.this);
+        Button btn=findViewById(R.id.btnOder);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                cart cart=new cart(Integer.valueOf(intent.getStringExtra("id")),1);
+                Log.d("Đặt hàng","thành công!");
+            }
+        });
+    }
+}
